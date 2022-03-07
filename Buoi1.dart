9675c04497facf9e86b8cbb2857732ca34ec5c62
NQT271 <nqt271@gmail.com>
2022-03-07 12:44:25 +0700

    1

---
+++ Buoi1.dart
@@ -0,0 +1,8 @@
+void main(){
+  var text1 = "Name: ";
+  var text2 = "Nguyen Quoc Toan";
+  // StringBuffer stringBuffer = StringBuffer();
+  // stringBuffer.write(text1);
+  // stringBuffer.write(text2);
+  print(text1 + "  " + text2);
+}
